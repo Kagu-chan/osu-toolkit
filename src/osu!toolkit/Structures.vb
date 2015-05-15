e61d6a682bcf63d9983c71b70334b00eb53b6811
Kai Böse <kai.boese@web.de>
2015-05-15 22:02:46 +0200

    Added Structures File

---
+++ src/osu!toolkit/Structures.vb
@@ -0,0 +1,23 @@
+﻿Option Compare Binary
+Option Explicit On
+Option Infer On
+Option Strict On
+
+Namespace osutoolkit
+
+    ''' <summary>
+    ''' Structure used for Filter Search in Mapsetprovider
+    ''' </summary>
+    ''' <remarks></remarks>
+    Public Structure MapsetProviderLoadArgument
+        Public filter As ProviderFilter
+        Public value As String
+
+        Sub New(providerFilter As ProviderFilter, value As String)
+            Me.filter = providerFilter
+            Me.value = value
+        End Sub
+
+    End Structure
+
+End Namespace
